f4f63a45210794c2c86ff737e39e9594d7725806
mihay <Yfc22nehwsz$>
2020-09-14 15:44:15 +0300

    Updated pub sub config

--- src/main/java/com/communityz/event/realtime/tamagochi/config/GcpPubSubConfig.java
+++ src/main/java/com/communityz/event/realtime/tamagochi/config/GcpPubSubConfig.java
@@ -3,7 +3,6 @@ package com.communityz.event.realtime.tamagochi.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
-import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
 import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,21 +16,6 @@ import org.springframework.messaging.MessageHandler;
 public class GcpPubSubConfig {
 
     @Bean
-    public PubSubInboundChannelAdapter messageChannelAdapter(
-            @Value("${spring.cloud.gcp.pub-sub.csv-processor-subscription-name}") String subscriptionName,
-            MessageChannel pubSubInputChannel,
-            PubSubTemplate pubSubTemplate
-    ) {
-        PubSubInboundChannelAdapter adapter =
-                new PubSubInboundChannelAdapter(pubSubTemplate, subscriptionName);
-        adapter.setOutputChannel(pubSubInputChannel);
-
-        log.debug("Subscribed for {}!", subscriptionName);
-
-        return adapter;
-    }
-
-    @Bean
     public MessageChannel pubSubInputChannel() {
         return new DirectChannel();
     }
@@ -39,7 +23,7 @@ public class GcpPubSubConfig {
     @Bean
     @ServiceActivator(inputChannel = "pubSubOutputChannel")
     public MessageHandler messageSender(
-            @Value("${spring.cloud.gcp.pub-sub.dataflow-json-processing-topic-name}") String topicName,
+            @Value("${spring.cloud.gcp.pub-sub.dataflow-topic}") String topicName,
             PubSubTemplate pubsubTemplate
     ) {
         return new PubSubMessageHandler(pubsubTemplate, topicName);
