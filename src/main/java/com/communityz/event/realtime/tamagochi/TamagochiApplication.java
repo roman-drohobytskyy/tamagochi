1612a0e3acc2c886265685ccd11c61225869b958
mihay <Yfc22nehwsz$>
2020-09-14 15:50:56 +0300

    Updated project Id

--- src/main/java/com/communityz/event/realtime/tamagochi/TamagochiApplication.java
+++ src/main/java/com/communityz/event/realtime/tamagochi/TamagochiApplication.java
@@ -2,7 +2,9 @@ package com.communityz.event.realtime.tamagochi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TamagochiApplication {
 
