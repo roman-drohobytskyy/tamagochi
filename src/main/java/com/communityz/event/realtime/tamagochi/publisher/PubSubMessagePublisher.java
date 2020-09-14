7050f6d888f7eb6f7eb1e56d2c84be96c982fdfa
mihay <Yfc22nehwsz$>
2020-09-14 12:51:17 +0300

    Initial publisher impl

---
+++ src/main/java/com/communityz/event/realtime/tamagochi/publisher/PubSubMessagePublisher.java
@@ -0,0 +1,9 @@
+package com.communityz.event.realtime.tamagochi.publisher;
+
+import org.springframework.integration.annotation.MessagingGateway;
+
+@MessagingGateway(defaultRequestChannel = "pubSubOutputChannel")
+public interface PubSubMessagePublisher {
+
+  void publish(String value);
+}
