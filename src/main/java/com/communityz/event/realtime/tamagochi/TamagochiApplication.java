d57605b90ef38cc64a9f45da89f94443a2b2708c
Roman Drohobytskyy <shatendri@gmail.com>
2020-09-14 12:34:48 +0300

    initial commit

---
+++ src/main/java/com/communityz/event/realtime/tamagochi/TamagochiApplication.java
@@ -0,0 +1,13 @@
+package com.communityz.event.realtime.tamagochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagochiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamagochiApplication.class, args);
+	}
+
+}
