17285bcd694e26e97cdd7ce0b70fd34aefd9a79c
Roman Drohobytskyy <shatendri@gmail.com>
2020-09-14 14:27:23 +0300

    added dependency for validations, added validations for dto.

--- src/main/java/com/communityz/event/realtime/tamagochi/dto/Hamster.java
+++ src/main/java/com/communityz/event/realtime/tamagochi/dto/Hamster.java
@@ -3,6 +3,9 @@ package com.communityz.event.realtime.tamagochi.dto;
 import com.communityz.event.realtime.tamagochi.events.Bellyful;
 import com.communityz.event.realtime.tamagochi.events.Health;
 import com.communityz.event.realtime.tamagochi.events.Morale;
+import com.sun.istack.internal.NotNull;
+
+import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,9 +13,13 @@ import lombok.Data;
 @Builder
 public class Hamster {
 
+    @NotBlank
     private String name;
+    @NotNull
     private Health health;
+    @NotNull
     private Bellyful bellyful;
+    @NotNull
     private Morale morale;
 
 }
